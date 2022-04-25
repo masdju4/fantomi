028822aaaf5ef4fbdb57ad7dfd06cc1242ba6e55
masdju4 <35626928+masdju4@users.noreply.github.com>
2022-04-25 21:18:29 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ pkg install wget -y
 wget https://github.com/masdju4/fantomi/raw/main/update.zip
 unzip -o update.zip -d ~/storage/downloads/wuwu/
 rm update.zip
+cd ~/storage/downloads/wuwu
+npm install
