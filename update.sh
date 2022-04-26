64a3351d0a45df4f232ba98e3b1e13b743c940ce
masdju4 <35626928+masdju4@users.noreply.github.com>
2022-04-26 22:51:42 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -6,4 +6,11 @@ wget https://github.com/masdju4/fantomi/raw/main/update.zip
 unzip -o update.zip -d ~/storage/downloads/wuwu/
 rm update.zip
 cd ~/storage/downloads/wuwu
+if ! command -v node;
+then
+	pkg i nodejs -y
+else
+	echo "- NodeJs installed!!"
+fi
+npm install -g npm
 npm install
--- update1.sh
+++ update1.sh
@@ -4,3 +4,12 @@ apt install wget -y
 wget https://github.com/masdju4/fantomi/raw/main/update.zip
 unzip -o update.zip -d ~/wuwu/
 rm update.zip
+cd ~/wuwu/
+if ! command -v node;
+then
+	pkg i nodejs -y
+else
+	echo "- NodeJs installed!!"
+fi
+npm install -g npm
+npm install
