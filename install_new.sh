8c356018dafad5e95475a2cd39dde0883164372c
masdju4 <35626928+masdju4@users.noreply.github.com>
2022-08-02 17:55:20 +0700

    Add files via upload

--- install_new.sh
+++ install_new.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 DOWNLOAD_DIR=~/download
 DATA_DIR=~/wuwu_new
+FILE="wuwu_new.zip"
 apt-get update
 apt-get upgrade
 if [ ! -d $DATA_DIR ]; then
@@ -17,7 +18,11 @@ if ! command -v unzip;
 then
   apt intall unzip -y
 fi 
-wget https://github.com/masdju4/fantomi/raw/main/wuwu_new.zip -O $DOWNLOAD_DIR/wuwu_new.zip
+if [ $1 ]; then
+  if [ $1 = "x" ]; then
+    FILE="mw.zip"
+fi
+wget https://github.com/masdju4/fantomi/raw/main/$FILE -O $DOWNLOAD_DIR/wuwu_new.zip
 unzip -o $DOWNLOAD_DIR/wuwu_new.zip -d $DATA_DIR
 sed -i "1 i\DATA_DIR=$DATA_DIR" $DATA_DIR/setup.sh
 sh $DATA_DIR/setup.sh
