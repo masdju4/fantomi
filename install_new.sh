fafcd1f461bf6b57b2b3354afdfe6143de6a21cd
masdju4 <35626928+masdju4@users.noreply.github.com>
2023-02-18 20:03:48 +0700

    Add files via upload

--- install_new.sh
+++ install_new.sh
@@ -20,11 +20,11 @@ then
 fi 
 if [ $1 ]; then
   if [ $1 = "x" ]; then
-    FILE="mw.zip"
+    FILE="w3.zip"
   fi
 fi
 wget https://github.com/masdju4/fantomi/raw/main/$FILE -O $DOWNLOAD_DIR/wuwu_new.zip
 unzip -o $DOWNLOAD_DIR/wuwu_new.zip -d $DATA_DIR
 sed -i "1 i\DATA_DIR=$DATA_DIR" $DATA_DIR/setup.sh
-sed -i "1 i\DATA_DIR=$DATA_DIR/log" $DATA_DIR/helper/log
+sed -i "1 i\DATA_DIR=$DATA_DIR" $DATA_DIR/log
 sh $DATA_DIR/setup.sh
